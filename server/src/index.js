838649efb843d642a95f88fa1082acf43faf68c3
Gyen Abubakar <indehyde@gmail.com>
2022-04-16 22:19:23 +0000

    connected resolvers and RESTDataSource to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,21 @@
 const fs = require("fs");
+const path = require("path");
 const { ApolloServer } = require('apollo-server');
+const resolvers = require('./resolvers')
+const TrackAPI = require('./datasources/track-api')
 
-const schema = fs.readFileSync(__dirname + '\\schema.graphql', {
+const schema = fs.readFileSync(path.join(__dirname, './schema.graphql'), {
   encoding: 'utf8'
 })
 
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(9)],
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-      };
-    },
-    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
-
 const server = new ApolloServer({
   typeDefs: schema,
-  mocks,
+  resolvers,
+  dataSources() {
+    return {
+      trackAPI: new TrackAPI()
+    }
+  }
 });
 
 server.listen().then(() => {
